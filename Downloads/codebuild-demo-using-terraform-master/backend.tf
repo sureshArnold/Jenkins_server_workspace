4f17d5c9ef02b5d872926346b073e1b05e4c1771
Suresh Kanuri <kanurisureshtheroy777@gmail.com>
2026-07-30 15:00:48 +0530

    Updated the repo

--- Downloads/codebuild-demo-using-terraform-master/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloud-terraform-basics"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile = true
-    encrypt        = true
-  }
-}
